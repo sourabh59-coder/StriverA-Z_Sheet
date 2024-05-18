42d8a00c22eddbd63908cb5b7e8050ad96217dbb
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-05-18 13:06:34 +0530

    Update Approach1.cpp

--- Step 10: Slidding Window & Two Pointer Problems/Lec 1: Medium Problems/1. Longest Substring without repeating characters/Approach1.cpp
+++ Step 10: Slidding Window & Two Pointer Problems/Lec 1: Medium Problems/1. Longest Substring without repeating characters/Approach1.cpp
@@ -1,1 +1,32 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+//Using the string directly and not using any type of data structure 
+int lengthOfLongestSubstring(string s)
+{
+    int n = s.length();
+    int left = 0;
+    int ans = 0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i-1;j>=left;j--)
+        {
+            if(s[i]==s[j])
+            {
+                left = j+1;
+                break;
+            }
+        }
+        ans = max(ans,i-left+1);
+    }
+    return ans;
+}
 
+signed main()
+{
+    string s;
+    cin>>s;
+    
+    cout<<lengthOfLongestSubstring(s)<<endl;
+    return 0;
+}
