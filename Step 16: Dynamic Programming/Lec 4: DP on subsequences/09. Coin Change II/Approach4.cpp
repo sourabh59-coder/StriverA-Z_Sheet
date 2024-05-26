f56740fab9a57f339bf2c425765f17f1adac8a58
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-05-26 14:09:23 +0530

    Create Approach4.cpp

---
+++ Step 16: Dynamic Programming/Lec 4: DP on subsequences/09. Coin Change II/Approach4.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    // int fun(vector<int> &v,int ind,int n,int k,vector<vector<int>> &dp)
+    // {
+    //     if(ind==n)
+    //     {
+    //         if(k==0)    return 1;
+    //         else        return 0;
+    //     }
+
+    //     if(dp[ind][k]!=-1)      return dp[ind][k];
+
+    //     int notTake = fun(v,ind+1,n,k,dp);
+
+    //     int Take = 0;
+
+    //     if(k >= v[ind])     Take = fun(v,ind,n,k-v[ind],dp);
+
+    //     return dp[ind][k] = (notTake + Take);
+    // }
+    int change(int k, vector<int>& v) {
+        int n = v.size();
+        // vector<vector<int>> dp(n+1, vector<int> (k+1,0));
+        vector<int> curr(k+1,0), next(k+1,0);
+
+        // return fun(v,0,n,k,dp);
+        // for(int i=0;i<=n;i++)   dp[i][0] = 1;
+        curr[0] = 1;
+        next[0] = 1;
+
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=k;j++)
+            {
+                int notTake = next[j];
+
+                int Take = 0;
+
+                if(j >= v[i])   Take = curr[j-v[i]];
+
+                curr[j] = (Take + notTake);
+            }
+
+            next = curr;
+        }
+
+        return curr[k];
+    }
+
+};   
