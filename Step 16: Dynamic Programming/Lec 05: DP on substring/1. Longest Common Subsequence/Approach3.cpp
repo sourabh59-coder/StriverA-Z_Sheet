da3b71b224e05211e25dc2b74ead3d8e5d0b81c2
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-05-27 11:09:28 +0530

    Create Approach3.cpp

---
+++ Step 16: Dynamic Programming/Lec 05: DP on substring/1. Longest Common Subsequence/Approach3.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    // int fun(string s1,string s2,int i,int j,int n,int m,vector<vector<int>> &dp)
+    // {
+    //     if(i==n || j==m)    return 0;
+
+    //     if(dp[i][j]!=-1)    return dp[i][j];
+
+    //     if(s1[i]==s2[j])    return dp[i][j] = (1 + fun(s1,s2,i+1,j+1,n,m,dp));
+
+    //     return dp[i][j] = max(fun(s1,s2,i+1,j,n,m,dp) , fun(s1,s2,i,j+1,n,m,dp));
+    // }
+    int longestCommonSubsequence(string& s1, string& s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        // vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
+        // vector<vector<int>> dp(n+1, vector<int> (m+1,0));
+        vector<int> curr(m+1,0), next(m+1,0);
+
+
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
+            {
+                if(s1[i] == s2[j])    curr[j] = (1 + next[j+1]);
+
+                else    curr[j] = max(next[j], curr[j+1]);
+            }
+
+            next = curr;
+        }
+
+        return curr[0];
+
+        // return fun(s1,s2,0,0,n,m,dp);
+    }
+};
