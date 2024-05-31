d58c269a1b90f7bc60c24a7e063af048e52dc54a
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-05-31 12:34:25 +0530

    Create Approach1.cpp

---
+++ Step 13: Binary Search Tree/Lec: 01: Traversal/12. Iterative PostOrder using 1 stack/Approach1.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+
+        TreeNode* curr = root;
+
+        vector<int> ans;
+
+        while(!st.empty()  || curr!=NULL)
+        {
+            if(curr!=NULL)
+            {
+                st.push(curr);
+                curr = curr->left;
+            }
+            else
+            {
+                TreeNode* temp = st.top()->right;
+
+                if(temp==NULL)
+                {
+                    while(!st.empty() && temp == st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+
+                else
+                {
+                    curr = temp;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
