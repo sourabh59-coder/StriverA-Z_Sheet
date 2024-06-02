095975a735e7431e895d1ebfe9ea0ca94d5d0a1d
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-02 22:06:09 +0530

    Create Approach2.cpp

---
+++ Step 16: Dynamic Programming/Lec 03: 2D-3D DP/07. 3-D DP/Approach2.cpp
@@ -0,0 +1,30 @@
+int fun(vector<vector<int>> &v,int i,int j1,int j2,int n,int m,vector<vector<vector<int>>> &dp)
+    {
+        if(j1<0 || j1>=m || j2<0 || j2>=m)     return -1e9;
+        if(i==n-1)
+        {
+            if(j1==j2)  return (v[i][j1]);
+            else        return (v[i][j1] + v[i][j2]);
+        }
+        // if(i==n)    return  0;
+        
+        if(dp[i][j1][j2]!=-1)   return dp[i][j1][j2];
+        
+        int maxi = -1e9;
+        
+        for(int j=-1;j<=1;j++)
+        {
+            for(int k=-1;k<=1;k++)
+            {
+                if(j1==j2)  maxi = max(maxi , v[i][j1] + fun(v,i+1,j1+j,j2+k,n,m,dp));
+                else        maxi = max(maxi , v[i][j1] + v[i][j2] + fun(v,i+1,j1+j,j2+k,n,m,dp));
+            }
+        }
+        
+        return dp[i][j1][j2] = maxi;
+    }
+    int solve(int n, int m, vector<vector<int>>& v) {
+        // code here
+        vector<vector<vector<int>>> dp(n, vector<vector<int>> (m, vector<int> (m,-1)));
+        return fun(v,0,0,m-1,n,m,dp);
+    }
