29b9e2946f9b1e23d4779650cf157821276d04ed
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-02 15:52:04 +0530

    Create Approach1.cpp

---
+++ Step 16: Dynamic Programming/Lec 03: 2D-3D DP/03. Grid Path II/Approach1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fun(vector<vector<int>> &v,int n,int m)
+    {
+        if(n==0 && m==0)    return 1;
+        if(n<0 || m<0 || v[n][m]==1)    return 0;
+
+        int left = fun(v,n-1,m);
+        int right = fun(v,n,m-1);
+
+        return (left + right);
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& v) {
+        int n = v.size();
+        int m = v[0].size();
+
+        return fun(v,n-1,m-1);
+    }
+};
