6e0e2a4ab2916ce5206f1fd0c2f66dd5be94aafb
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-02 15:54:24 +0530

    Update Approach1.cpp

--- Step 16: Dynamic Programming/Lec 03: 2D-3D DP/03. Grid Path II/Approach1.cpp
+++ Step 16: Dynamic Programming/Lec 03: 2D-3D DP/03. Grid Path II/Approach1.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int fun(vector<vector<int>> &v,int n,int m)
     {
-        if(n==0 && m==0)    return 1;
         if(n<0 || m<0 || v[n][m]==1)    return 0;
+        if(n==0 && m==0)    return 1;
+        
 
         int left = fun(v,n-1,m);
         int right = fun(v,n,m-1);
