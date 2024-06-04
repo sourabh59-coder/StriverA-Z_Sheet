df1e564ecb220279cb03f0dfaac33cd3873dfc80
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-04 14:49:27 +0530

    Create Approach1.cpp

---
+++ Step 13: Binary Search Tree/Lec 02: Medium Problem/10. Bottom Level View/Approach1.cpp
@@ -0,0 +1,36 @@
+vector <int> bottomView(Node *root) {
+        // Your Code Here
+        map<int,int> mp;
+        queue<pair<Node*, int>> qe;
+        
+        if(root)    qe.push({root,0});
+        
+        while(!qe.empty())
+        {
+            int si = qe.size();
+            
+            for(int i=0;i<si;i++)
+            {
+                auto it = qe.front();
+                qe.pop();
+                
+                auto node = it.first;
+                int level = it.second;
+                
+                mp[level] = node->data;
+                
+                if(node->left)  qe.push({node->left,level-1});
+                
+                if(node->right) qe.push({node->right,level+1});
+            }
+        }
+        
+        vector<int> ans;
+        
+        for(auto it=mp.begin();it!=mp.end();it++)
+        {
+            ans.push_back(it->second);
+        }
+        
+        return ans;
+    }
