d7e60799379e67613f68e8090740769be7997819
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-07 16:08:37 +0530

    Update Approach1.cpp

--- Step 16: Dynamic Programming/Lec 08: MCM/01. Matrix chain multiplication/Approach1.cpp
+++ Step 16: Dynamic Programming/Lec 08: MCM/01. Matrix chain multiplication/Approach1.cpp
@@ -1,21 +1,49 @@
-int fun(int v[],int i,int j,vector<vector<int>> &dp)
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int fun(int v[],int i,int j)
     {
         if(i==j)    return 0;
-        
-        if(dp[i][j]!=-1)    return dp[i][j];
         
         int ans = INT_MAX;
         for(int k=i;k<=j-1;k++)
         {
-            int steps = v[i-1]*v[k]*v[j] + fun(v,i,k,dp) + fun(v,k+1,j,dp);
+            int steps = v[i-1]*v[k]*v[j] + fun(v,i,k) + fun(v,k+1,j);
             ans = min(ans,steps);
         }
         
-        return dp[i][j] = ans;
+        return ans;
     }
     int matrixMultiplication(int n, int v[])
     {
         // code here
-        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
-        return fun(v,1,n-1,dp);
+        return fun(v,1,n-1);
     }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.matrixMultiplication(N, arr)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
