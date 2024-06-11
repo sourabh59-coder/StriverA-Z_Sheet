b037602850fd49e19ca090d8bbdac4bc25d7e39d
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-11 22:40:56 +0530

    Create Approach1.cpp

---
+++ Step 04: Binary Search/Lec 2: BS on Answers/01. Square root/Approach1.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+  
+
+// } Driver Code Ends
+// Function to find square root
+// x: element to find square root
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        // Your code goes here   
+        long long int temp = 1;
+        long long l = 0, r = x;
+        
+        while(l<=r)
+        {
+            long long mid = l + (r-l)/2;
+            
+            if(mid*mid == x)    return mid;
+            else if(mid*mid < x)
+            {
+                temp = mid;
+                l = mid+1;
+            }
+            else    r = mid-1;
+        }
+        
+        return temp;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		Solution obj;
+		cout << obj.floorSqrt(n) << endl;
+	}
+    return 0;   
+}
+
+// } Driver Code Ends
