0f3f5d29f8c7e26444aab751269afd94ca5d1e2f
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-12 23:02:36 +0530

    Create Approach2.cpp

---
+++ Step 07: Recursion/Lec 02: Subsequence Patterns/03. Print all subsequences/Approach2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& v) {
+        vector<vector<int>> ans;
+
+        int n = v.size();
+        
+        for(int i=0;i<pow(2,n);i++)
+        {
+            vector<int> vec;
+            for(int j=0;j<n;j++)
+            {
+                if(i & (1<<j))
+                {
+                    vec.push_back(v[j]);
+                }
+            }
+            ans.push_back(vec);
+        }
+
+        return ans;
+    }
+};
