0af429b2a264cd805847f5e79e8e322922517a51
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-15 20:09:46 +0530

    Create Approach1.cpp

---
+++ Step 03: Array/Lec 03: Hard/01. Pascal/Approach1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> ans;
+        ans.push_back({1});
+        if(n==1)    return ans;
+        ans.push_back({1,1});
+        for(int i=2;i<n;i++)
+        {
+            vector<int> v;
+            v.push_back(1);
+            for(int j=0;j<i-1;j++)
+            {
+                v.push_back(ans[i-1][j] + ans[i-1][j+1]);
+            }
+            v.push_back(1);
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
