11f01735c52c33cc749984f381358ae826c1c5ac
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-16 20:34:01 +0530

    Create Approach1.cpp

---
+++ Step 8 : Bit Manipulation [Concepts & Problems]/Lec 3: Advanced Maths/04. Prime Factorization/Approach1.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void sieve() {}
+
+    vector<int> findPrimeFactors(int n) {
+
+        // Write your code here
+        vector<int> ans;
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                while(n%i==0)
+                {
+                    ans.push_back(i);
+                    n = n/i;
+                }
+            }
+        }
+        if(n>1) ans.push_back(n);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        int n;
+        cin >> n;
+
+        Solution obj;
+        obj.sieve();
+        vector<int> vec = obj.findPrimeFactors(n);
+        for (int i = 0; i < vec.size(); i++) {
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
