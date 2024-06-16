bfee2ca0c06761eee407b9ab3f3e7422e2a379f3
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-16 19:42:49 +0530

    Create Approach1.cpp

---
+++ Step 8 : Bit Manipulation [Concepts & Problems]/Lec 3: Advanced Maths/01. Prime Factors/Approach1.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+	public:
+	vector<int>AllPrimeFactors(int n) {
+	    // Code here
+	    vector<int> ans;
+	    int temp = n;
+	    for(int i=2;i*i<=n;i++)
+	    {
+	        if(n%i==0)
+	        {
+	            ans.push_back(i);
+	            while(n%i==0)   n = n/i;
+	        }
+	    }
+	    if(n!=1)    ans.push_back(n);
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		long long int N;
+		cin >> N;
+		Solution ob;
+		vector<int>ans = ob.AllPrimeFactors(N);
+		for(auto i: ans)
+			cout << i <<" ";
+		cout <<"\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
