3441c6c646c1a987f2254226c4f29bad6ee692ec
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-18 11:29:52 +0530

    Create Approach1.cpp

---
+++ Step 03: Array/Lec 02: Medium/08. Next permutation/Approach1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& v) {
+        int ind = -1;
+        int n = v.size();
+        for(int i=n-2;i>=0;i--)
+        {
+            if(v[i]<v[i+1])
+            {
+                ind = i;
+                break;
+            }
+        }
+
+        if(ind==-1) reverse(v.begin(),v.end());
+        else
+        {
+            for(int i=n-1;i>=0;i--)
+            {
+                if(v[i]>v[ind])
+                {
+                    swap(v[i],v[ind]);
+                    break;
+                }
+            }
+
+            reverse(v.begin()+ind+1,v.end());
+        }
+    }
+};
