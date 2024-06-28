6009b5e4ad1539e780a0dd5ff4b935ed3e1f0f8c
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-28 12:41:12 +0530

    Create Approach1.cpp

---
+++ Step 13: Binary Search Tree/Lec 03: Hard Problem/12. Morris preorder/Approach1.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+
+        TreeNode* curr = root;
+
+        while(curr!=NULL)
+        {
+            if(curr->left==NULL)
+            {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }    
+            else
+            {
+                TreeNode* temp = curr->left;
+                while(temp->right!=NULL && temp->right!=curr)
+                {
+                    temp = temp->right;
+                }
+
+                if(temp->right==NULL)
+                {
+                    temp->right = curr;
+                    curr = curr->left;
+                }
+                else
+                {
+                    temp->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
