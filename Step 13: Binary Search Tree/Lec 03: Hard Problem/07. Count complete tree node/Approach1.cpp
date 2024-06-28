fa67362b8cd5d7d67a85486e379dfd63065ef904
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-28 11:53:53 +0530

    Create Approach1.cpp

---
+++ Step 13: Binary Search Tree/Lec 03: Hard Problem/07. Count complete tree node/Approach1.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int leftHeight(TreeNode* root)
+    {
+        int cnt = 0;
+        while(root)
+        {
+            cnt++;
+            root = root->left;
+        }
+        return cnt;
+    }
+    int rightHeight(TreeNode* root)
+    {
+        int cnt = 0;
+        while(root)
+        {
+            cnt++;
+            root = root->right;
+        }
+        return cnt;
+    }
+    int countNodes(TreeNode* root) {
+        if(root==NULL)  return 0;
+
+        int  l = leftHeight(root);
+        int  r = rightHeight(root);
+
+        if(l==r)    return (1<<l) - 1;
+
+        return countNodes(root->left) + countNodes(root->right) + 1;
+    }
+};
