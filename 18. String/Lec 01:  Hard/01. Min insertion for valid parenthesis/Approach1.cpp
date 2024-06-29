699a94701b2ad158110087d203318ea4970b0250
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-06-29 17:10:23 +0530

    Create Approach1.cpp

---
+++ 18. String/Lec 01:  Hard/01. Min insertion for valid parenthesis/Approach1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int n = s.length();
+        stack<char> st;
+        for(int i=0;i<n;i++)
+        {
+            if(st.empty()  || s[i]=='(')    st.push(s[i]);
+            else
+            {
+                if(st.top()=='(')   st.pop();
+                else                st.push(s[i]);
+            }
+        }
+        return st.size();
+    }
+};
